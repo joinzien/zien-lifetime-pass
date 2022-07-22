4aade4d69fe19fc0fd8cedd02be998c90abcc14a
Zoe Nolan <zoe@zien.io>
2022-07-22 16:08:23 +0100

    Add only owners test

---
+++ test/MinterManagementTest.ts
@@ -0,0 +1,127 @@
+import { expect } from "chai";
+import "@nomiclabs/hardhat-ethers";
+import { ethers, deployments } from "hardhat";
+import parseDataURI from "data-urls";
+
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {
+  DropCreator,
+  ExpandedNFT,
+} from "../typechain";
+
+describe("MinterManagement", () => { 
+  let signer: SignerWithAddress;
+  let signerAddress: string;
+
+  let artist: SignerWithAddress;
+  let artistAddress: string;  
+
+  let dynamicSketch: DropCreator;
+
+  beforeEach(async () => {
+    const { DropCreator } = await deployments.fixture([
+      "DropCreator",
+      "ExpandedNFT",
+    ]);
+    const dynamicMintableAddress = (
+      await deployments.get("ExpandedNFT")
+    ).address;
+    dynamicSketch = (await ethers.getContractAt(
+      "DropCreator",
+      DropCreator.address
+    )) as DropCreator;
+
+    signer = (await ethers.getSigners())[0];
+    signerAddress = await signer.getAddress();
+
+    artist = (await ethers.getSigners())[1];
+    artistAddress = await signer.getAddress();
+
+  });
+
+  describe("with a drop", () => {
+    let signer1: SignerWithAddress;
+    let minterContract: ExpandedNFT;
+    beforeEach(async () => {
+      signer1 = (await ethers.getSigners())[1];
+      await dynamicSketch.createDrop(
+        artistAddress,
+        "Testing Token",
+        "TEST",
+        10,
+        ["This is a testing token for all","This is a testing token for all","This is a testing token for all","This is a testing token for all","This is a testing token for all",
+        "This is a testing token for all","This is a testing token for all","This is a testing token for all","This is a testing token for all","This is a testing token for all"],
+        ["https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+          "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+          "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+          "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy"],
+        ["0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000"],
+        ["", "", "", "", "", "", "", "", "", ""],
+        ["0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000"]
+      );
+
+      const dropResult = await dynamicSketch.getDropAtId(0);
+      minterContract = (await ethers.getContractAt(
+        "ExpandedNFT",
+        dropResult
+      )) as ExpandedNFT;
+
+      minterContract.setPricing(10, 500, 0, 0, 0, 4, 4, 4);       
+    });
+
+    it("Contract owner user access control", async () => {
+      minterContract.setApprovedVIPMinters(1, [signerAddress], [true]);
+
+      minterContract.setAllowedMinter(0);
+
+      // Mint as a contract owner
+      await expect(minterContract.mintEdition(signerAddress))
+        .to.emit(minterContract, "Transfer")
+        .withArgs(
+          "0x0000000000000000000000000000000000000000",
+          signerAddress,
+          1
+        );
+
+      minterContract.setAllowedMinter(1);
+
+      // Mint as a VIP
+      await expect(minterContract.mintEdition(signerAddress))
+        .to.emit(minterContract, "Transfer")
+        .withArgs(
+          "0x0000000000000000000000000000000000000000",
+          signerAddress,
+          2
+        );
+
+      minterContract.setAllowedMinter(2);
+
+      // Mint as a Member
+      await expect(minterContract.mintEdition(signerAddress))
+        .to.emit(minterContract, "Transfer")
+        .withArgs(
+          "0x0000000000000000000000000000000000000000",
+          signerAddress,
+          3
+        );
+
+        minterContract.setAllowedMinter(3);
+
+      // Mint as the general public
+      await expect(minterContract.mintEdition(signerAddress))
+        .to.emit(minterContract, "Transfer")
+        .withArgs(
+          "0x0000000000000000000000000000000000000000",
+          signerAddress,
+          4
+        );        
+    });  
+  });
+});
