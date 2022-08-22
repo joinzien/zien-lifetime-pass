4ed71887e900e303584b6d315d76fef2c5b6f8b6
Zoe Nolan <zoe@zien.io>
2022-08-22 23:22:16 +0100

    Image test

--- test/SharedNFTLogicTest.ts
+++ test/SharedNFTLogicTest.ts
@@ -228,5 +228,105 @@ describe("SharedNFTLogicTest", () => {
         properties: { number: 1, name: "Testing Token" },
       })
     );
-  });  
+  });
+  
+  it("makes a new drop with an image and an animation", async () => {
+    await dynamicSketch.createDrop(
+      artistAddress,
+      "Testing Token",
+      "TEST",
+      10, // 1% royalty since BPS  
+    );
+
+    const dropResult = await dynamicSketch.getDropAtId(0);
+    const minterContract = (await ethers.getContractAt(
+      "ExpandedNFT",
+      dropResult
+    )) as ExpandedNFT;
+
+    await minterContract.loadMetadataChunk(
+      1, 10,      
+        ["This is a testing token for all", "This is a testing token for all", "This is a testing token for all", "This is a testing token for all", "This is a testing token for all",
+        "This is a testing token for all", "This is a testing token for all", "This is a testing token for all", "This is a testing token for all", "This is a testing token for all"],
+        ["https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn24w43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy"], 
+      ["0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000"],
+        ["https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy",
+        "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy", "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy"],        
+      ["0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "0x0000000000000000000000000000000000000000000000000000000000000000"]
+    );
+
+    const mintCost = ethers.utils.parseEther("0.1");      
+    await minterContract.setPricing(10, 500, mintCost, mintCost, mintCost, 15, 15, 15);
+
+    await minterContract.setAllowedMinter(3);
+
+    // Mint first edition
+    await expect(minterContract.mintEdition(signerAddress, {
+      value: ethers.utils.parseEther("0.1")
+    }))
+      .to.emit(minterContract, "Transfer")
+      .withArgs(
+        "0x0000000000000000000000000000000000000000",
+        signerAddress,
+        1
+      );
+
+    expect(await minterContract.name()).to.be.equal("Testing Token");
+    expect(await minterContract.symbol()).to.be.equal("TEST");
+    const dropUris = await minterContract.getURIs(1);
+    expect(dropUris[0]).to.be.equal(
+      "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy"
+    );
+    expect(dropUris[1]).to.be.equal(
+      "0x0000000000000000000000000000000000000000000000000000000000000000"
+    );
+    expect(dropUris[2]).to.be.equal("https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn4xw43jcy4knwful7ymmgy");
+    expect(dropUris[3]).to.be.equal(
+      "0x0000000000000000000000000000000000000000000000000000000000000000"
+    );
+
+    expect(await minterContract.dropSize()).to.be.equal(10);
+    expect(await minterContract.owner()).to.be.equal(signerAddress);
+
+    const tokenURI = await minterContract.tokenURI(1);
+
+    const parsedTokenURI = parseDataURI(tokenURI);
+    if (!parsedTokenURI) {
+      throw "No parsed token uri";
+    }
+
+    // Check metadata from edition
+    const uriData = Buffer.from(parsedTokenURI.body).toString("utf-8");
+    const metadata = JSON.parse(uriData);
+
+    expect(parsedTokenURI.mimeType.type).to.equal("application");
+    expect(parsedTokenURI.mimeType.subtype).to.equal("json");
+    // expect(parsedTokenURI.mimeType.parameters.get("charset")).to.equal(
+    //   "utf-8"
+    // );
+    expect(JSON.stringify(metadata)).to.equal(
+      JSON.stringify({
+        name: "Testing Token 1/10",
+        description: "This is a testing token for all",
+        image:
+          "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy?id=1",
+        animation_url:
+          "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy?id=1",          
+        properties: { number: 1, name: "Testing Token" },
+      })
+    );
+  });    
 });
