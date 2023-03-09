6edd938bde80fb7168c2fb2fafee8d25597e594b
Zoe Nolan <zoe@zien.io>
2023-03-09 14:15:23 +0000

    Consolidate all the inting tests into a single suite

--- test/MintTest.ts
+++ test/MintTest.ts
@@ -157,4 +157,59 @@ describe("Mint Limits", () => {
     
     //expect(await minterContract.totalSupply()).to.be.equal(1);
   });  
+
+  it("General user access control", async () => {
+    let user = (await ethers.getSigners())[2];
+    let userAddress = await user.getAddress();
+
+    await dynamicSketch.createDrop(
+      artistAddress,
+      "Testing Token",
+      "TEST",
+      10);
+
+    const dropResult = await dynamicSketch.getDropAtId(0);
+    const minterContract = (await ethers.getContractAt(
+      "ExpandedNFT",
+      dropResult
+    )) as ExpandedNFT;
+      
+    await minterContract.loadMetadataChunk(
+      1, 10,
+      ["http://example.com/token/01", "http://example.com/token/02", 
+       "http://example.com/token/03", "http://example.com/token/04", 
+       "http://example.com/token/05", "http://example.com/token/06", 
+       "http://example.com/token/07", "http://example.com/token/08", 
+       "http://example.com/token/09", "http://example.com/token/10"]
+    );
+
+    const mintCost = ethers.utils.parseEther("0.1");
+
+    await minterContract.setPricing(10, 500, mintCost, mintCost, 2, 1);
+
+    await minterContract.setAllowedMinter(0);
+
+    // Mint as a contract owner
+    await expect(minterContract.connect(user).mintEdition(userAddress)).to.be.revertedWith("Needs to be an allowed minter");      
+
+    await minterContract.setAllowedMinter(1);
+
+    // Mint as a member of the allow list
+    await expect(minterContract.connect(user).mintEdition(userAddress)).to.be.revertedWith("Needs to be an allowed minter");   
+
+    await minterContract.setAllowedMinter(2);
+
+    // Mint as the general public
+    await expect(minterContract.connect(user).mintEdition(userAddress, {
+      value: ethers.utils.parseEther("0.1")
+    }))
+      .to.emit(minterContract, "Transfer")
+      .withArgs(
+        "0x0000000000000000000000000000000000000000",
+        userAddress,
+        1
+      );
+      
+    expect(await minterContract.totalSupply()).to.be.equal(1);
+  });   
 });
