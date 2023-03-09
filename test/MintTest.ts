fbad671b532df49dbe216f80b86d46b86893b57c
Zoe Nolan <zoe@zien.io>
2023-03-09 15:56:00 +0000

    Refactor the mint test to reduce the duplicated code

--- test/MintTest.ts
+++ test/MintTest.ts
@@ -12,44 +12,47 @@ import {
   ExpandedNFT,
 } from "../typechain";
 
-describe("Mint Limits", () => {
+describe("Mint", () => {
   let signer: SignerWithAddress;
   let signerAddress: string;
 
   let artist: SignerWithAddress;
   let artistAddress: string;    
 
+  let user: SignerWithAddress;
+  let userAddress: string; 
+
   let dynamicSketch: DropCreator;
+  let minterContract: ExpandedNFT;
 
   beforeEach(async () => {
+    signer = (await ethers.getSigners())[0];
+    signerAddress = await signer.getAddress();
+
+    artist = (await ethers.getSigners())[1];
+    artistAddress = await signer.getAddress();   
+    
+    user = (await ethers.getSigners())[2];
+    userAddress = await user.getAddress();
+
     const { DropCreator } = await deployments.fixture([
       "DropCreator",
       "ExpandedNFT",
     ]);
-    const dynamicMintableAddress = (
-      await deployments.get("ExpandedNFT")
-    ).address;
+
     dynamicSketch = (await ethers.getContractAt(
       "DropCreator",
       DropCreator.address
     )) as DropCreator;
 
-    signer = (await ethers.getSigners())[0];
-    signerAddress = await signer.getAddress();
-
-    artist = (await ethers.getSigners())[1];
-    artistAddress = await signer.getAddress();    
-  });
-
-  it("General mint limit", async () => {
     await dynamicSketch.createDrop(
       artistAddress,
       "Testing Token",
       "TEST",
       10);
 
-    const dropResult = await dynamicSketch.getDropAtId(0);
-    const minterContract = (await ethers.getContractAt(
+    const dropResult = await dynamicSketch.getDropAtId(0);   
+    minterContract = (await ethers.getContractAt(
       "ExpandedNFT",
       dropResult
     )) as ExpandedNFT;
@@ -63,8 +66,11 @@ describe("Mint Limits", () => {
        "http://example.com/token/09", "http://example.com/token/10"]
     );
 
-    await minterContract.setPricing(10, 500, 0, 0, 2, 1);
+    const mintCost = ethers.utils.parseEther("0.1");
+    await minterContract.setPricing(10, 500, mintCost, mintCost, 2, 1);   
+  });
 
+  it("General mint limit", async () => {
     expect(await minterContract.getAllowListMintLimit()).to.be.equal(2);
     expect(await minterContract.getGeneralMintLimit()).to.be.equal(1);
     expect(await minterContract.getMintLimit(signerAddress)).to.be.equal(0); 
@@ -98,95 +104,8 @@ describe("Mint Limits", () => {
     expect(await minterContract.canMint(signerAddress)).to.be.equal(false);  
 
   });
-  
-  it("Allow list mint limit", async () => {
-    await dynamicSketch.createDrop(
-      artistAddress,
-      "Testing Token",
-      "TEST",
-      10);
-
-    const dropResult = await dynamicSketch.getDropAtId(0);
-    const minterContract = (await ethers.getContractAt(
-      "ExpandedNFT",
-      dropResult
-    )) as ExpandedNFT;
-      
-    await minterContract.loadMetadataChunk(
-      1, 10,
-      ["http://example.com/token/01", "http://example.com/token/02", 
-       "http://example.com/token/03", "http://example.com/token/04", 
-       "http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10"]
-    );
-
-    //await minterContract.setPricing(10, 500, 0, 0, 1, 2);
-    //await minterContract.reserve ([signerAddress], [1]) 
-
-    //expect(await minterContract.getAllowListMintLimit()).to.be.equal(1);
-    //expect(await minterContract.getGeneralMintLimit()).to.be.equal(2);
-    //expect(await minterContract.getMintLimit(signerAddress)).to.be.equal(0);    
-
-    //expect(await minterContract.canMint(signerAddress)).to.be.equal(false);  
-
-    //await expect(minterContract.purchase()).to.be.revertedWith("Not for sale");
-    
-    //expect(
-    //  await minterContract.setAllowListSalePrice(ethers.utils.parseEther("0.2"))
-    //).to.emit(minterContract, "PriceChanged");
-
-    //expect(await minterContract.getAllowListMintLimit()).to.be.equal(1);
-    //expect(await minterContract.getGeneralMintLimit()).to.be.equal(2);
-    //expect(await minterContract.getMintLimit(signerAddress)).to.be.equal(1);    
-
-    //expect(await minterContract.canMint(signerAddress)).to.be.equal(true);  
-
-    //expect(
-    //  await minterContract
-    //    .purchase({ value: ethers.utils.parseEther("0.2") })
-    //).to.emit(minterContract, "EditionSold");
-
-    //expect(await minterContract.getAllowListMintLimit()).to.be.equal(1);
-    //expect(await minterContract.getGeneralMintLimit()).to.be.equal(2);
-    //expect(await minterContract.getMintLimit(signerAddress)).to.be.equal(0);       
-
-    //expect(await minterContract.canMint(signerAddress)).to.be.equal(false);  
-
-    //await expect(minterContract.purchase({ value: ethers.utils.parseEther("0.2") })).to.be.revertedWith( "Exceeded mint limit");
-    
-    //expect(await minterContract.totalSupply()).to.be.equal(1);
-  });  
 
   it("General user access control", async () => {
-    let user = (await ethers.getSigners())[2];
-    let userAddress = await user.getAddress();
-
-    await dynamicSketch.createDrop(
-      artistAddress,
-      "Testing Token",
-      "TEST",
-      10);
-
-    const dropResult = await dynamicSketch.getDropAtId(0);
-    const minterContract = (await ethers.getContractAt(
-      "ExpandedNFT",
-      dropResult
-    )) as ExpandedNFT;
-      
-    await minterContract.loadMetadataChunk(
-      1, 10,
-      ["http://example.com/token/01", "http://example.com/token/02", 
-       "http://example.com/token/03", "http://example.com/token/04", 
-       "http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10"]
-    );
-
-    const mintCost = ethers.utils.parseEther("0.1");
-
-    await minterContract.setPricing(10, 500, mintCost, mintCost, 2, 1);
-
     await minterContract.setAllowedMinter(0);
 
     // Mint as a contract owner
