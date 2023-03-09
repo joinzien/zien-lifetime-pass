8ea978c50c6c5b553e9ce6c0e22ed2b6465096a1
Zoe Nolan <zoe@zien.io>
2023-03-09 09:40:20 +0000

    Check more of the tests

--- test/WhoCanMintTest.ts
+++ test/WhoCanMintTest.ts
@@ -66,22 +66,5 @@ describe("Who Can Mint", () => {
     await minterContract.setAllowedMinter(2);
 
     expect(await minterContract.getAllowedMinter()).to.be.equal(2);
-
-    expect(await minterContract.name()).to.be.equal("Testing Token");
-    expect(await minterContract.symbol()).to.be.equal("TEST");
-    const dropUris = await minterContract.getURIs(1);
-    expect(dropUris[0]).to.be.equal("");
-    expect(dropUris[1]).to.be.equal(
-      "0x0000000000000000000000000000000000000000000000000000000000000000"
-    );
-    expect(dropUris[2]).to.be.equal(
-      "https://ipfs.io/ipfsbafybeify52a63pgcshhbtkff4nxxxp2zp5yjn2xw43jcy4knwful7ymmgy"
-    );
-    expect(dropUris[3]).to.be.equal(
-      "0x0000000000000000000000000000000000000000000000000000000000000000"
-    );
-    expect(await minterContract.dropSize()).to.be.equal(10);
-    expect(await minterContract.totalSupply()).to.be.equal(0);
-    expect(await minterContract.owner()).to.be.equal(signerAddress);
   });
 });
