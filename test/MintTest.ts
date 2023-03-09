c2ddda9c057865f0ecdcb30680048ecae43ba937
Zoe Nolan <zoe@zien.io>
2023-03-09 18:03:44 +0000

    Test users can not mint when not on sale

--- test/MintTest.ts
+++ test/MintTest.ts
@@ -114,6 +114,12 @@ describe("Mint", () => {
     expect(await minterContract.getMintLimit(signerAddress)).to.be.equal(0);  
   });
 
+  it("Can not mint while the drop is not for sale", async () => {
+    await minterContract.setAllowedMinter(0);
+
+    await expect(minterContract.mintEditions([signerAddress], { value: ethers.utils.parseEther("0.1") })).to.be.revertedWith("Not for sale");
+  });
+
   it("General mint limit", async () => {
     expect(await minterContract.getAllowListMintLimit()).to.be.equal(2);
     expect(await minterContract.getGeneralMintLimit()).to.be.equal(1);
