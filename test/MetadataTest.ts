18681c19bdee45e871d181943a06001e0e0ac9c5
Zoe Nolan <zoe@zien.io>
2023-03-09 20:04:26 +0000

    Add missing test for updating the redeemed metadata

--- test/MetadataTest.ts
+++ test/MetadataTest.ts
@@ -61,12 +61,12 @@ describe("Metadata", () => {
     )) as ExpandedNFT;
 
     await expect(minterContract.connect(artist).loadMetadataChunk(
-        1, 10,
-        ["http://example.com/token/01", "http://example.com/token/02", 
-         "http://example.com/token/03", "http://example.com/token/04", 
-         "http://example.com/token/05", "http://example.com/token/06", 
-         "http://example.com/token/07", "http://example.com/token/08", 
-         "http://example.com/token/09", "http://example.com/token/10"]
+      1, 10,
+      ["http://example.com/token/01", "http://example.com/token/02",
+        "http://example.com/token/03", "http://example.com/token/04",
+        "http://example.com/token/05", "http://example.com/token/06",
+        "http://example.com/token/07", "http://example.com/token/08",
+        "http://example.com/token/09", "http://example.com/token/10"]
     )).to.be.revertedWith("Ownable: caller is not the owner");
 
     const metadataLoaded = await minterContract.metadataloaded();
@@ -88,11 +88,11 @@ describe("Metadata", () => {
 
     await expect(minterContract.loadMetadataChunk(
       0, 10,
-      ["http://example.com/token/01", "http://example.com/token/02", 
-       "http://example.com/token/03", "http://example.com/token/04", 
-       "http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10"]
+      ["http://example.com/token/01", "http://example.com/token/02",
+        "http://example.com/token/03", "http://example.com/token/04",
+        "http://example.com/token/05", "http://example.com/token/06",
+        "http://example.com/token/07", "http://example.com/token/08",
+        "http://example.com/token/09", "http://example.com/token/10"]
     )).to.be.revertedWith("StartIndex > 0");
 
     const metadataLoaded = await minterContract.metadataloaded();
@@ -114,11 +114,11 @@ describe("Metadata", () => {
 
     await expect(minterContract.loadMetadataChunk(
       2, 10,
-      ["http://example.com/token/01", "http://example.com/token/02", 
-       "http://example.com/token/03", "http://example.com/token/04", 
-       "http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10"]
+      ["http://example.com/token/01", "http://example.com/token/02",
+        "http://example.com/token/03", "http://example.com/token/04",
+        "http://example.com/token/05", "http://example.com/token/06",
+        "http://example.com/token/07", "http://example.com/token/08",
+        "http://example.com/token/09", "http://example.com/token/10"]
     )).to.be.revertedWith("Data large than drop size");
 
     const metadataLoaded = await minterContract.metadataloaded();
@@ -140,11 +140,11 @@ describe("Metadata", () => {
 
     await minterContract.loadMetadataChunk(
       1, 10,
-      ["http://example.com/token/01", "http://example.com/token/02", 
-       "http://example.com/token/03", "http://example.com/token/04", 
-       "http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10"]
+      ["http://example.com/token/01", "http://example.com/token/02",
+        "http://example.com/token/03", "http://example.com/token/04",
+        "http://example.com/token/05", "http://example.com/token/06",
+        "http://example.com/token/07", "http://example.com/token/08",
+        "http://example.com/token/09", "http://example.com/token/10"]
     );
 
     const metadataLoaded = await minterContract.metadataloaded();
@@ -166,20 +166,20 @@ describe("Metadata", () => {
 
     await minterContract.loadMetadataChunk(
       1, 10,
-      ["http://example.com/token/01", "http://example.com/token/02", 
-       "http://example.com/token/03", "http://example.com/token/04", 
-       "http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10"]
+      ["http://example.com/token/01", "http://example.com/token/02",
+        "http://example.com/token/03", "http://example.com/token/04",
+        "http://example.com/token/05", "http://example.com/token/06",
+        "http://example.com/token/07", "http://example.com/token/08",
+        "http://example.com/token/09", "http://example.com/token/10"]
     );
 
     await minterContract.loadMetadataChunk(
       11, 10,
-      ["http://example.com/token/11", "http://example.com/token/12", 
-       "http://example.com/token/13", "http://example.com/token/14", 
-       "http://example.com/token/15", "http://example.com/token/16", 
-       "http://example.com/token/17", "http://example.com/token/18", 
-       "http://example.com/token/19", "http://example.com/token/20"]
+      ["http://example.com/token/11", "http://example.com/token/12",
+        "http://example.com/token/13", "http://example.com/token/14",
+        "http://example.com/token/15", "http://example.com/token/16",
+        "http://example.com/token/17", "http://example.com/token/18",
+        "http://example.com/token/19", "http://example.com/token/20"]
     );
   });
 
@@ -198,29 +198,29 @@ describe("Metadata", () => {
 
     await minterContract.loadMetadataChunk(
       1, 10,
-      ["http://example.com/token/01", "http://example.com/token/02", 
-       "http://example.com/token/03", "http://example.com/token/04", 
-       "http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10"]
+      ["http://example.com/token/01", "http://example.com/token/02",
+        "http://example.com/token/03", "http://example.com/token/04",
+        "http://example.com/token/05", "http://example.com/token/06",
+        "http://example.com/token/07", "http://example.com/token/08",
+        "http://example.com/token/09", "http://example.com/token/10"]
     );
 
     await minterContract.loadMetadataChunk(
       5, 10,
-      ["http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10", 
-       "http://example.com/token/11", "http://example.com/token/12", 
-       "http://example.com/token/13", "http://example.com/token/14"]
+      ["http://example.com/token/05", "http://example.com/token/06",
+        "http://example.com/token/07", "http://example.com/token/08",
+        "http://example.com/token/09", "http://example.com/token/10",
+        "http://example.com/token/11", "http://example.com/token/12",
+        "http://example.com/token/13", "http://example.com/token/14"]
     );
 
     await minterContract.loadMetadataChunk(
       11, 10,
-      ["http://example.com/token/11", "http://example.com/token/12", 
-       "http://example.com/token/13", "http://example.com/token/14", 
-       "http://example.com/token/15", "http://example.com/token/16", 
-       "http://example.com/token/17", "http://example.com/token/18", 
-       "http://example.com/token/19", "http://example.com/token/20"]
+      ["http://example.com/token/11", "http://example.com/token/12",
+        "http://example.com/token/13", "http://example.com/token/14",
+        "http://example.com/token/15", "http://example.com/token/16",
+        "http://example.com/token/17", "http://example.com/token/18",
+        "http://example.com/token/19", "http://example.com/token/20"]
     );
 
     const metadataLoaded = await minterContract.metadataloaded();
@@ -242,20 +242,20 @@ describe("Metadata", () => {
 
     await minterContract.loadMetadataChunk(
       1, 10,
-      ["http://example.com/token/01", "http://example.com/token/02", 
-       "http://example.com/token/03", "http://example.com/token/04", 
-       "http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10"]
+      ["http://example.com/token/01", "http://example.com/token/02",
+        "http://example.com/token/03", "http://example.com/token/04",
+        "http://example.com/token/05", "http://example.com/token/06",
+        "http://example.com/token/07", "http://example.com/token/08",
+        "http://example.com/token/09", "http://example.com/token/10"]
     );
 
     await minterContract.loadMetadataChunk(
       5, 10,
-      ["http://example.com/token/05", "http://example.com/token/06", 
-       "http://example.com/token/07", "http://example.com/token/08", 
-       "http://example.com/token/09", "http://example.com/token/10", 
-       "http://example.com/token/11", "http://example.com/token/12", 
-       "http://example.com/token/13", "http://example.com/token/14"]
+      ["http://example.com/token/05", "http://example.com/token/06",
+        "http://example.com/token/07", "http://example.com/token/08",
+        "http://example.com/token/09", "http://example.com/token/10",
+        "http://example.com/token/11", "http://example.com/token/12",
+        "http://example.com/token/13", "http://example.com/token/14"]
     );
 
     const metadataLoaded = await minterContract.metadataloaded();
@@ -278,9 +278,6 @@ describe("Metadata", () => {
     await expect(minterContract.connect(artist).loadRedeemedMetadata(
       1, "https://example.com/redeemed/0001"
     )).to.be.revertedWith("Ownable: caller is not the owner");
-
-    const metadataLoaded = await minterContract.metadataloaded();
-    expect(metadataLoaded).to.be.equal(false);
   });
 
   it("Load redeemed metadata below the starting index", async () => {
@@ -299,9 +296,6 @@ describe("Metadata", () => {
     await expect(minterContract.loadRedeemedMetadata(
       0, "https://example.com/redeemed/0001"
     )).to.be.revertedWith("tokenID > 0");
-
-    const metadataLoaded = await minterContract.metadataloaded();
-    expect(metadataLoaded).to.be.equal(false);
   });
 
   it("Load redeemed metadata over the ending index", async () => {
@@ -320,8 +314,23 @@ describe("Metadata", () => {
     await expect(minterContract.loadRedeemedMetadata(
       11, "https://example.com/redeemed/0001"
     )).to.be.revertedWith("tokenID <= drop size");
+  });
 
-    const metadataLoaded = await minterContract.metadataloaded();
-    expect(metadataLoaded).to.be.equal(false);
+  it("Load redeemed metadata", async () => {
+    await dynamicSketch.createDrop(
+      artistAddress,
+      "Testing Token",
+      "TEST",
+      10);
+
+    const dropResult = await dynamicSketch.getDropAtId(0);
+    minterContract = (await ethers.getContractAt(
+      "ExpandedNFT",
+      dropResult
+    )) as ExpandedNFT;
+
+    await minterContract.loadRedeemedMetadata(
+      1, "https://example.com/redeemed/0001"
+    );
   });
 });
