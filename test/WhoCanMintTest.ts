e091b6b0882bc4821112e5063b87fdc0aac8d02a
Zoe Nolan <zoe@zien.io>
2023-03-12 19:02:41 +0000

    Test setting who can mint

--- test/WhoCanMintTest.ts
+++ test/WhoCanMintTest.ts
@@ -65,4 +65,68 @@ describe("Who Can Mint", () => {
 
     expect(await minterContract.getAllowedMinter()).to.be.equal(2);
   });
+
+  it("makes a new drop, not as the owner", async () => {
+    const artist = (await ethers.getSigners())[1];
+    const artistAddress = await artist.getAddress();
+
+    await dynamicSketch.createDrop(
+      artistAddress,
+      "Testing Token",
+      "TEST",
+      10);
+
+    const dropResult = await dynamicSketch.getDropAtId(0);
+    const minterContract = (await ethers.getContractAt(
+      "ExpandedNFT",
+      dropResult
+    )) as ExpandedNFT;
+
+    await minterContract.loadMetadataChunk(
+      1, 10,
+      ["http://example.com/token/01", "http://example.com/token/02", 
+       "http://example.com/token/03", "http://example.com/token/04", 
+       "http://example.com/token/05", "http://example.com/token/06", 
+       "http://example.com/token/07", "http://example.com/token/08", 
+       "http://example.com/token/09", "http://example.com/token/10"]
+    );
+
+    await minterContract.setPricing(10, 500, 10, 10, 1, 1);
+
+    expect(await minterContract.getAllowedMinter()).to.be.equal(0);
+
+    await expect(minterContract.connect(artist).setAllowedMinter(2)).to.be.revertedWith("Ownable: caller is not the owner");  
+  }); 
+  
+  it("Set an invalid minter", async () => {
+    const artist = (await ethers.getSigners())[1];
+    const artistAddress = await artist.getAddress();
+
+    await dynamicSketch.createDrop(
+      artistAddress,
+      "Testing Token",
+      "TEST",
+      10);
+
+    const dropResult = await dynamicSketch.getDropAtId(0);
+    const minterContract = (await ethers.getContractAt(
+      "ExpandedNFT",
+      dropResult
+    )) as ExpandedNFT;
+
+    await minterContract.loadMetadataChunk(
+      1, 10,
+      ["http://example.com/token/01", "http://example.com/token/02", 
+       "http://example.com/token/03", "http://example.com/token/04", 
+       "http://example.com/token/05", "http://example.com/token/06", 
+       "http://example.com/token/07", "http://example.com/token/08", 
+       "http://example.com/token/09", "http://example.com/token/10"]
+    );
+
+    await minterContract.setPricing(10, 500, 10, 10, 1, 1);
+
+    expect(await minterContract.getAllowedMinter()).to.be.equal(0);
+
+    await expect(minterContract.setAllowedMinter(3)).to.be.revertedWith("");
+  });   
 });
