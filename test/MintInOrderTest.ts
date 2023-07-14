5fae9983b4cf8220a204c132da9dbe3299e97c69
Zoe Nolan <zoe@zien.io>
2023-07-14 01:50:07 +0100

    Test minting too many

--- test/MintInOrderTest.ts
+++ test/MintInOrderTest.ts
@@ -13,161 +13,185 @@ import {
 } from "../typechain";
 
 describe("Mint in order", () => {
-    let signer: SignerWithAddress;
-    let signerAddress: string;
-  
-    let artist: SignerWithAddress;
-    let artistAddress: string;    
-  
-    let user: SignerWithAddress;
-    let userAddress: string; 
-  
-    let dynamicSketch: DropCreator;
-    let minterContract: OpenEditionsNFT;
-  
-    beforeEach(async () => {
-      signer = (await ethers.getSigners())[0];
-      signerAddress = await signer.getAddress();
-  
-      artist = (await ethers.getSigners())[1];
-      artistAddress = await artist.getAddress();   
-      
-      user = (await ethers.getSigners())[2];
-      userAddress = await user.getAddress();
-  
-      const { DropCreator } = await deployments.fixture([
-        "DropCreator",
-        "OpenEditionsNFT",
-      ]);
-  
-      dynamicSketch = (await ethers.getContractAt(
-        "DropCreator",
-        DropCreator.address
-      )) as DropCreator;
-  
-      await dynamicSketch.createDrop(
-        artistAddress,
-        "Testing Token",
-        "TEST",
-        "http://example.com/token/",
-        10, 1, false);
-  
-      const dropResult = await dynamicSketch.getDropAtId(0);   
-      minterContract = (await ethers.getContractAt(
-        "OpenEditionsNFT",
-        dropResult
-      )) as OpenEditionsNFT;
-  
-      const mintCost = ethers.utils.parseEther("0.1");
-      await minterContract.setPricing(10, 500, mintCost, mintCost, 2, 1);   
-    });
-  
-    it("Mint as not the owner with drop not on sale", async () => {
-      await minterContract.setAllowedMinter(0);
+  let signer: SignerWithAddress;
+  let signerAddress: string;
 
-      expect(await minterContract.canMint(userAddress)).to.be.equal(false);      
-  
-      await expect(minterContract.connect(user).mintEdition(userAddress)).to.be.revertedWith("Needs to be an allowed minter");      
-         
-      expect(await minterContract.totalSupply()).to.be.equal(0);
-      expect(await minterContract.isRandomMint()).to.be.equal(false);
-    });  
+  let artist: SignerWithAddress;
+  let artistAddress: string;
 
-    it("Mint as not the owner with drop limited to the allow list", async () => { 
-      await minterContract.setAllowedMinter(1);
+  let user: SignerWithAddress;
+  let userAddress: string;
 
-      expect(await minterContract.canMint(userAddress)).to.be.equal(false);
-  
-      await expect(minterContract.connect(user).mintEdition(userAddress)).to.be.revertedWith("Needs to be an allowed minter");   
-        
-      expect(await minterContract.totalSupply()).to.be.equal(0);
-      expect(await minterContract.isRandomMint()).to.be.equal(false);
-    });  
+  let dynamicSketch: DropCreator;
+  let minterContract: OpenEditionsNFT;
 
-    it("Mint as not the owner with drop omn general release", async () => {
-      await minterContract.setAllowedMinter(2);
+  beforeEach(async () => {
+    signer = (await ethers.getSigners())[0];
+    signerAddress = await signer.getAddress();
 
-      expect(await minterContract.canMint(userAddress)).to.be.equal(true);
+    artist = (await ethers.getSigners())[1];
+    artistAddress = await artist.getAddress();
 
-      await expect(minterContract.connect(user).mintEdition(userAddress, {
-        value: ethers.utils.parseEther("0.1")
-      }))
-        .to.emit(minterContract, "Transfer")
-        .withArgs(
-          "0x0000000000000000000000000000000000000000",
-          userAddress,
-          1
-        );
-        
-      expect(await minterContract.totalSupply()).to.be.equal(1);
-      expect(await minterContract.isRandomMint()).to.be.equal(false);
-    });  
+    user = (await ethers.getSigners())[2];
+    userAddress = await user.getAddress();
 
-    it("Mint with the wrong payment amount", async () => {
-      await minterContract.setAllowedMinter(2);
+    const { DropCreator } = await deployments.fixture([
+      "DropCreator",
+      "OpenEditionsNFT",
+    ]);
 
-      expect(await minterContract.canMint(userAddress)).to.be.equal(true);
+    dynamicSketch = (await ethers.getContractAt(
+      "DropCreator",
+      DropCreator.address
+    )) as DropCreator;
 
-      await expect(minterContract.connect(user).mintEdition(userAddress)).to.be.revertedWith("Wrong price");  
-        
-      expect(await minterContract.totalSupply()).to.be.equal(0);
-      expect(await minterContract.isRandomMint()).to.be.equal(false);
-    });  
+    await dynamicSketch.createDrop(
+      artistAddress,
+      "Testing Token",
+      "TEST",
+      "http://example.com/token/",
+      10, 1, false);
 
-    it("Mint as owner with drop not on sale", async () => {
-      await minterContract.setAllowedMinter(0);
-  
-      expect(await minterContract.canMint(signerAddress)).to.be.equal(true);
+    const dropResult = await dynamicSketch.getDropAtId(0);
+    minterContract = (await ethers.getContractAt(
+      "OpenEditionsNFT",
+      dropResult
+    )) as OpenEditionsNFT;
 
-      await expect(minterContract.mintEdition(userAddress, {
-        value: ethers.utils.parseEther("0.0")
-      }))
-        .to.emit(minterContract, "Transfer")
-        .withArgs(
-          "0x0000000000000000000000000000000000000000",
-          userAddress,
-          1
-        );    
-         
-        expect(await minterContract.totalSupply()).to.be.equal(1);
-        expect(await minterContract.isRandomMint()).to.be.equal(false);
-    });  
+    const mintCost = ethers.utils.parseEther("0.1");
+    await minterContract.setPricing(10, 500, mintCost, mintCost, 2, 1);
+  });
 
-    it("Mint as the owner with drop limited to the allow list", async () => { 
-      await minterContract.setAllowedMinter(1);
-  
-      expect(await minterContract.canMint(signerAddress)).to.be.equal(true);
+  it("Mint as not the owner with drop not on sale", async () => {
+    await minterContract.setAllowedMinter(0);
 
-      await expect(minterContract.mintEdition(userAddress, {
-        value: ethers.utils.parseEther("0.1")
-      }))
-        .to.emit(minterContract, "Transfer")
-        .withArgs(
-          "0x0000000000000000000000000000000000000000",
-          userAddress,
-          1
-        );   
-        
-      expect(await minterContract.totalSupply()).to.be.equal(1);
-      expect(await minterContract.isRandomMint()).to.be.equal(false);
-    });  
+    expect(await minterContract.canMint(userAddress)).to.be.equal(false);
 
-    it("Mint as the owner with drop omn general release", async () => {
-      await minterContract.setAllowedMinter(2);
+    await expect(minterContract.connect(user).mintEdition(userAddress)).to.be.revertedWith("Needs to be an allowed minter");
 
-      expect(await minterContract.canMint(signerAddress)).to.be.equal(true);
-  
-      await expect(minterContract.mintEdition(userAddress, {
-        value: ethers.utils.parseEther("0.1")
-      }))
-        .to.emit(minterContract, "Transfer")
-        .withArgs(
-          "0x0000000000000000000000000000000000000000",
-          userAddress,
-          1
-        );   
-        
-      expect(await minterContract.totalSupply()).to.be.equal(1);
-      expect(await minterContract.isRandomMint()).to.be.equal(false);
-    }); 
+    expect(await minterContract.totalSupply()).to.be.equal(0);
+    expect(await minterContract.isRandomMint()).to.be.equal(false);
+  });
+
+  it("Mint as not the owner with drop limited to the allow list", async () => {
+    await minterContract.setAllowedMinter(1);
+
+    expect(await minterContract.canMint(userAddress)).to.be.equal(false);
+
+    await expect(minterContract.connect(user).mintEdition(userAddress)).to.be.revertedWith("Needs to be an allowed minter");
+
+    expect(await minterContract.totalSupply()).to.be.equal(0);
+    expect(await minterContract.isRandomMint()).to.be.equal(false);
+  });
+
+  it("Mint as not the owner with drop omn general release", async () => {
+    await minterContract.setAllowedMinter(2);
+
+    expect(await minterContract.canMint(userAddress)).to.be.equal(true);
+
+    await expect(minterContract.connect(user).mintEdition(userAddress, {
+      value: ethers.utils.parseEther("0.1")
+    }))
+      .to.emit(minterContract, "Transfer")
+      .withArgs(
+        "0x0000000000000000000000000000000000000000",
+        userAddress,
+        1
+      );
+
+    expect(await minterContract.totalSupply()).to.be.equal(1);
+    expect(await minterContract.isRandomMint()).to.be.equal(false);
+  });
+
+  it("Try to mint too many", async () => {
+    await minterContract.setAllowedMinter(2);
+
+    expect(await minterContract.canMint(userAddress)).to.be.equal(true);
+
+    await expect(minterContract.connect(user).mintEdition(userAddress, {
+      value: ethers.utils.parseEther("0.1")
+    }))
+      .to.emit(minterContract, "Transfer")
+      .withArgs(
+        "0x0000000000000000000000000000000000000000",
+        userAddress,
+        1
+      );
+
+    expect(await minterContract.totalSupply()).to.be.equal(1);
+
+    await expect(minterContract.connect(user).mintEdition(userAddress, {
+      value: ethers.utils.parseEther("0.1")
+    })).to.be.revertedWith("Exceeded mint limit");
+
+    expect(await minterContract.totalSupply()).to.be.equal(1);
+  });
+
+  it("Mint with the wrong payment amount", async () => {
+    await minterContract.setAllowedMinter(2);
+
+    expect(await minterContract.canMint(userAddress)).to.be.equal(true);
+
+    await expect(minterContract.connect(user).mintEdition(userAddress)).to.be.revertedWith("Wrong price");
+
+    expect(await minterContract.totalSupply()).to.be.equal(0);
+    expect(await minterContract.isRandomMint()).to.be.equal(false);
+  });
+
+  it("Mint as owner with drop not on sale", async () => {
+    await minterContract.setAllowedMinter(0);
+
+    expect(await minterContract.canMint(signerAddress)).to.be.equal(true);
+
+    await expect(minterContract.mintEdition(userAddress, {
+      value: ethers.utils.parseEther("0.0")
+    }))
+      .to.emit(minterContract, "Transfer")
+      .withArgs(
+        "0x0000000000000000000000000000000000000000",
+        userAddress,
+        1
+      );
+
+    expect(await minterContract.totalSupply()).to.be.equal(1);
+    expect(await minterContract.isRandomMint()).to.be.equal(false);
+  });
+
+  it("Mint as the owner with drop limited to the allow list", async () => {
+    await minterContract.setAllowedMinter(1);
+
+    expect(await minterContract.canMint(signerAddress)).to.be.equal(true);
+
+    await expect(minterContract.mintEdition(userAddress, {
+      value: ethers.utils.parseEther("0.1")
+    }))
+      .to.emit(minterContract, "Transfer")
+      .withArgs(
+        "0x0000000000000000000000000000000000000000",
+        userAddress,
+        1
+      );
+
+    expect(await minterContract.totalSupply()).to.be.equal(1);
+    expect(await minterContract.isRandomMint()).to.be.equal(false);
+  });
+
+  it("Mint as the owner with drop omn general release", async () => {
+    await minterContract.setAllowedMinter(2);
+
+    expect(await minterContract.canMint(signerAddress)).to.be.equal(true);
+
+    await expect(minterContract.mintEdition(userAddress, {
+      value: ethers.utils.parseEther("0.1")
+    }))
+      .to.emit(minterContract, "Transfer")
+      .withArgs(
+        "0x0000000000000000000000000000000000000000",
+        userAddress,
+        1
+      );
+
+    expect(await minterContract.totalSupply()).to.be.equal(1);
+    expect(await minterContract.isRandomMint()).to.be.equal(false);
+  });
 });
